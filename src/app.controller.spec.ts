238fd17bb2a6fbee5a4d9f2e4de5ff3f77aba9f9
rtondev <rton.dev@gmail.com>
2026-06-26 14:32:28 -0300

    feat: new page /

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "RP API — backend em execução."', () => {
+      expect(appController.getHello()).toBe('RP API — backend em execução.');
     });
   });
 });
